89b3bd3df74c55d654df19fd66fc12fb953feaee
VarunSaraswathibatla <61550359+VarunSaraswathibatla@users.noreply.github.com>
2021-04-14 18:19:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const dialogflow = require('@google-cloud/dialogflow');
 const uuid = require('uuid');
 
-/**
- * Send a query to the dialogflow agent, and return the query result.
- * @param {string} projectId The project to be used
- */
+
 async function runSample(projectId = 'varun-assistant-el9c') {
-  // A unique identifier for the given session
+ 
   const sessionId = uuid.v4();
 
-  // Create a new session
+  
   const sessionClient = new dialogflow.SessionsClient({
-    keyFilename:"C:/Users/Lenovo/Desktop/ChatDesk/varun-assistant-el9c-a35a360b0081.json"
+    keyFilename:Mongod request manager;
   });                    
       
       
 
   const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
 
-  // The text query request.
+  
   const request = {
     session: sessionPath,
     queryInput: {
       text: {
-        // The query to send to the dialogflow agent
+     
         text: 'I am bored?',
         // The language used by the client (en-US)
         languageCode: 'en-US',
@@ -31,7 +28,7 @@ async function runSample(projectId = 'varun-assistant-el9c') {
     },
   };
 
-  // Send request and log result
+  
   const responses = await sessionClient.detectIntent(request);
   console.log('Detected intent');
   const result = responses[0].queryResult;
