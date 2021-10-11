137ce2afa8e0119afa7b734c1ca0e13ee96ac345
VarunSaraswathibatla <61550359+VarunSaraswathibatla@users.noreply.github.com>
2021-10-11 20:03:01 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
 const dialogflow = require('@google-cloud/dialogflow');
 const uuid = require('uuid');
-const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const bodyParser = require("body-parser")
+const app = express();
+const port = 5000;
+const sessionId = uuid.v4();
 
-var URL = "mongodb://localhost:27017/";
-var random = require("random-string");
-async function runSample(projectId = 'varun-assistant-el9c') {
- 
-  const sessionId = uuid.v4();
+app.use(bodyParser.urlencoded({
+  extended : false
+}))
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post('/send-msg',(req,res)=>{
+  runSample(req.body.MSG).then(data=>{
+    res.send({Reply:data})
+  })
+})
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'varun-assistant-el9c') {
+  // A unique identifier for the given session
   
-  const sessionClient = new dialogflow.SessionsClient({});                    
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+    keyFilename:"C:/Users/Lenovo/Desktop/ChatDesk/varun-assistant-el9c-a35a360b0081.json"
+  });                    
       
       
 
   const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
 
-  
+  // The text query request.
   const request = {
     session: sessionPath,
     queryInput: {
       text: {
-     
-        text: 'I am bored?',
+        // The query to send to the dialogflow agent
+        text: msg,
         // The language used by the client (en-US)
         languageCode: 'en-US',
       },
     },
   };
 
-  
+  // Send request and log result
   const responses = await sessionClient.detectIntent(request);
   console.log('Detected intent');
   const result = responses[0].queryResult;
@@ -39,5 +68,9 @@ async function runSample(projectId = 'varun-assistant-el9c') {
   } else {
     console.log(`  No intent matched.`);
   }
+  return result.fulfillmentText;
 }
-runSample()
+
+app.listen(port,()=>{
+  console.log("Running on Port: "+port)
+})
